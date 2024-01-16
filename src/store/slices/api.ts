a29063c597624c104fbb01a8ba0378268017fd2c
Rafig Hajili <hacili.rafiq.03@gmail.com>
2024-01-16 20:44:01 +0400

    c

--- src/store/slices/api.ts
+++ src/store/slices/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://connectify-mz4i.onrender.com",
+    baseUrl: "https://api.connectify.az",
     credentials: "include",
   }),
   endpoints: () => ({}),
